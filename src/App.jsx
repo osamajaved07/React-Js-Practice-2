de634b1afae8889d5a12fdbce9c02e78cbb681d6
Osama Javed <osamajaved2002@gmail.com>
2025-05-19 01:55:07 +0500

    19-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [num, setnum] = useState(2);
   return (
     <div className="p-12">
-      <h1 className="text-xl">Number is {num}</h1>
+      <h1 className="text-xl bg-blue-200 px-6 py-6 ">Number is {num}</h1>
       <button onClick={() => setnum(num + 1)} className="bg-zinc-800 text-white px-8 py-2 my-4">
         Increment
       </button>
