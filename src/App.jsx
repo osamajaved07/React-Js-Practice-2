743103f51846919492a1d7dac79e46a5a12505c2
Osama Javed <osamajaved2002@gmail.com>
2025-05-19 01:50:32 +0500

    19-05-25

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [num, setnum] = useState(2);
+  return (
+    <div className="p-12">
+      <h1 className="text-xl">Number is {num}</h1>
+      <button onClick={() => setnum(num + 1)} className="bg-zinc-800 text-white px-8 py-2 my-4">
+        Increment
+      </button>
+      <button onClick={() => setnum(num - 1)} className="bg-zinc-800 text-white px-8 py-2 my-4 mx-2">
+        Decrement
+      </button>
+      <button onClick={() => setnum(2)} className="bg-zinc-800 text-white px-8 py-2 my-4 mx-2 ">
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default App;
