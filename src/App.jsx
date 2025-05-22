d59736d265b0f4932f7a77b9c84c6c8f3dea5984
Osama Javed <osamajaved2002@gmail.com>
2025-05-22 15:18:29 +0500

    22-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -78,12 +78,25 @@
 
 // export default App;
 
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+  };
   return (
     <div className="min-h-screen bg-[#282828] p-10">
-      <button className="bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90">Get data</button>;
+      <button onClick={getData} className="bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90">
+        Get data
+      </button>
+      <div className="p-5 mt-5 bg-gray-950 text-white">
+        {data.map(function (elem, idx) {
+          return <div key={idx} className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3"></div>;
+        })}
+      </div>
     </div>
   );
 };
