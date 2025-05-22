4f7b7669f7b20ebac25203e790d7be2aeee014ab
Osama Javed <osamajaved2002@gmail.com>
2025-05-22 14:28:16 +0500

    22-05-25

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,6 +11,7 @@ const Card = (props) => {
         {props.city}, {props.age}
       </h2>
       <button className="mt-5 bg-emerald-700 text-white px-4 py-2 rounded font-medium">Add friend</button>
+      <button className="mt-5 ml-2 bg-red-800 text-white px-4 py-2 rounded font-medium">Delete</button>
     </div>
   );
 };
