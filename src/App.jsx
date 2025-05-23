494ebeec986811bcef39b0b390fe482faf9f8155
Osama Javed <osamajaved2002@gmail.com>
2025-05-24 01:59:58 +0500

    22-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -78,36 +78,52 @@
 
 // export default App;
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+
+// const App = () => {
+//   const [data, setData] = useState([]);
+//   const getData = async () => {
+//     const response = await axios.get("https://picsum.photos/v2/list");
+//     setData(response.data);
+//     console.log(response.data);
+//   };
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   return (
+//     <div className="min-h-screen bg-[#282828] p-10">
+//       {/* <button onClick={getData} className="bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90">
+//         Get data
+//       </button> */}
+//       <div className="p-5 mt-5 bg-gray-950 text-white">
+//         {data.map(function (elem, idx) {
+//           return (
+//             <div key={idx} className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3">
+//               <img className="h-40 " src={elem.download_url} alt="" />
+//               <h1>{elem.author}</h1>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list");
-    setData(response.data);
-    console.log(response.data);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
-    <div className="min-h-screen bg-[#282828] p-10">
-      {/* <button onClick={getData} className="bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90">
-        Get data
-      </button> */}
-      <div className="p-5 mt-5 bg-gray-950 text-white">
-        {data.map(function (elem, idx) {
-          return (
-            <div key={idx} className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3">
-              <img className="h-40 " src={elem.download_url} alt="" />
-              <h1>{elem.author}</h1>
-            </div>
-          );
-        })}
-      </div>
+    <div>
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 };
