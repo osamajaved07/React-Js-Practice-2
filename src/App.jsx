e346293ee35dba728c09e7be871cbc654ae09ea7
Osama Javed <osamajaved2002@gmail.com>
2025-05-24 21:28:12 +0500

    24-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -117,12 +117,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </div>
   );
